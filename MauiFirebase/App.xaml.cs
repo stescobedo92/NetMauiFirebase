d4b5f3e186bfb72e290f350cff8fabb55925d2a9
stescobedo <stescobedo.31@gmail.com>
2023-01-17 21:04:23 -0600

    initial structure

---
+++ MauiFirebase/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiFirebase;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiFirebase/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiFirebase;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
