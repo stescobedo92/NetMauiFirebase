7dd24589aba359a5b502838cda59bcfad5461e07
stescobedo <stescobedo.31@gmail.com>
2023-01-17 21:51:30 -0600

    Remove unnecesary files

--- MauiFirebase/MauiProgram.cs
+++ MauiFirebase/MauiProgram.cs
@@ -7,13 +7,12 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
-
+		builder.UseMauiApp<App>()
+			   .ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+				
 		return builder.Build();
 	}
 }
